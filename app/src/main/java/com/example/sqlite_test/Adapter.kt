f63795e7435d21403ed4f9d4d85548992ae9da67
goodideas <goodideas@goodideasde-MacBook-Pro.local>
2020-05-12 08:05:14 +0800

    Add Adapter

---
+++ app/src/main/java/com/example/sqlite_test/Adapter.kt
@@ -0,0 +1,60 @@
+package com.example.sqlite_test
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.itemview.view.*
+
+class Adapter(var ct:Context,var outerList: MutableList<Int>) : RecyclerView.Adapter<Adapter.ViewHolder>(){
+
+    var innerList = mutableListOf<Int>()
+
+
+
+    init {
+
+        innerList = outerList
+
+
+    }
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Adapter.ViewHolder {
+
+        val view = LayoutInflater.from(ct).inflate(R.layout.itemview,parent,false)
+
+        return ViewHolder(view)
+
+    }
+
+    override fun getItemCount(): Int = innerList.size
+
+    override fun onBindViewHolder(holder: Adapter.ViewHolder, position: Int) {
+
+        holder.setUI(position)
+
+    }
+
+    inner class ViewHolder(view: View):RecyclerView.ViewHolder(view){
+
+        val num_text = view.num_text
+
+        fun setUI(position: Int){
+
+            num_text.text = innerList[position].toString()
+
+        }
+
+    }
+
+    fun update (list: MutableList<Int>){
+
+        innerList = list
+
+        notifyDataSetChanged()
+
+    }
+}
